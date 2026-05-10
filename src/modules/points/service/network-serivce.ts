17c9f2aa5e94c555bb7e312ecf2505c9fc416e2c
Henrique <henrique@MacBookPro.home>
2026-05-10 11:12:18 -0300

    feature: criacao service de rede

---
+++ src/modules/points/service/network-serivce.ts
@@ -0,0 +1,80 @@
+import { Point, PointType } from "../../points/entity/Point";
+import { PointsRepository } from "../../points/repository/points-repository";
+import { calculateDistanceInMeters } from "../../../shared/utils/calculate-distance";
+
+interface NetworkConnection {
+  clientId: string;
+  poleId: string;
+  distance: number;
+}
+
+interface GenerateNetworkOutput {
+  connections: NetworkConnection[];
+}
+
+export class NetworkService {
+  private pointsRepository: PointsRepository;
+
+  constructor() {
+    this.pointsRepository = new PointsRepository();
+  }
+
+  async generateNetwork(): Promise<GenerateNetworkOutput> {
+    const points = await this.pointsRepository.findAll();
+
+    const clients = points.filter((point) => point.type === PointType.Client);
+    const poles = points.filter((point) => point.type === PointType.Pole);
+
+    const connections = clients.map((client) => {
+      const nearestPole = this.findNearestPole(client, poles);
+
+      return {
+        clientId: client.id,
+        poleId: nearestPole.pole.id,
+        distance: nearestPole.distance,
+      };
+    });
+
+    return { connections };
+  }
+
+  private findNearestPole(
+    client: Point,
+    poles: Point[]
+  ): { pole: Point; distance: number } {
+    let nearestPole = poles[0];
+    let shortestDistance = calculateDistanceInMeters(
+      {
+        latitude: client.latitude,
+        longitude: client.longitude,
+      },
+      {
+        latitude: nearestPole.latitude,
+        longitude: nearestPole.longitude,
+      }
+    );
+
+    for (const pole of poles.slice(1)) {
+      const distance = calculateDistanceInMeters(
+        {
+          latitude: client.latitude,
+          longitude: client.longitude,
+        },
+        {
+          latitude: pole.latitude,
+          longitude: pole.longitude,
+        }
+      );
+
+      if (distance < shortestDistance) {
+        shortestDistance = distance;
+        nearestPole = pole;
+      }
+    }
+
+    return {
+      pole: nearestPole,
+      distance: shortestDistance,
+    };
+  }
+}
